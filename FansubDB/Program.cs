e2bc74a151eba3dcdbf9ef708b7b63a6c1f51eac
Inareous <septian.wijaya@mail.ugm.ac.id>
2018-03-08 20:44:00 +0700

    Planned Nyan-API. To be fixed by Author later

--- FansubDB/Program.cs
+++ FansubDB/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
 using LiteDB;
+using Newtonsoft.Json;
 
 namespace FansubDB
 {
@@ -15,9 +20,21 @@ namespace FansubDB
             var db = new LiteDatabase(@"MyData.db"); // unused for now
             Console.WriteLine("Starting Crawler");
             const int startIndex = 1;
-            const int endIndex = 5;
+            const int endIndex = 2;
             Scraper.Scrape("https://oploverz.id", startIndex,endIndex);
-            Console.WriteLine("Updating Database");
+            Console.WriteLine("Creating Nyan-API Call");
+            #region Populating Links To Call
+//            jsonOBJ json = new jsonOBJ();
+//            foreach (var entry in Entries)
+//            foreach (var type in entry.Download.FileType)
+//            foreach (var link in type.Link)
+//            json.urlArray.Add(link.Url);
+//            string JSON_Data = JsonConvert.SerializeObject(json);
+//            var result = jsonOBJ.NyanApiCall(JSON_Data);
+//            var resultOBJ = JsonConvert.DeserializeObject<jsonOBJ>(result);
+//            Console.WriteLine(resultOBJ);
+            #endregion
+            Console.WriteLine("Updating Links");
             var linkCount = 0;
             foreach (var entry in Entries)
             foreach (var type in entry.Download.FileType)
@@ -25,6 +42,32 @@ namespace FansubDB
                 linkCount++;
             Console.WriteLine($"Total Link : {linkCount}");
             Console.ReadLine();
+        }
+    }
+
+    internal class jsonOBJ
+    {
+        public List<string> urlArray = new List<string>();
+
+        public static string NyanApiCall(string json)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://mafuyu-bypasser.herokuapp.com/bypass");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream() ?? throw new InvalidOperationException()))
+            {
+                var result = streamReader.ReadToEnd();
+                return result;
+            }
         }
     }
 }
